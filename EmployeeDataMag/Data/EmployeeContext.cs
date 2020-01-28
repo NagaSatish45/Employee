ccf9483c7601caa80a308eb0485732393712ae15
NagaSatish45 <nagasatish45@gmail.com>
2020-01-28 13:26:12 +0530

    1st

---
+++ EmployeeDataMag/Data/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeDataMag.Models;
+
+namespace EmployeeDataMag.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext (DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeDataMag.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ EmployeeDataMag/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace EmployeeDataMag.Models
+{
+    public class Employee
+    {
+       
+        public int id { set; get; }
+        [Required]
+        public String name { set; get; }
+        [Required]
+        public string Email{ set; get; }
+        public branch branch { set; get; }
+
+    }
+    public enum branch
+  { 
+        it,cse,Mech,eee
+    
+    } 
+
+}
